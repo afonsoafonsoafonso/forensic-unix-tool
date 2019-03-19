85ef139ab080a2826300f9509a8aa0de0465187c
Leonor Sousa <leonormsousa@sapo.pt>
2019-03-19 16:02:54 +0000

    Small changes

--- forensic.c
+++ forensic.c
@@ -7,8 +7,14 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <string.h>
 
 int main(int argc, char* argv[])
 {
+    if (argc < 2)
+    {
+        printf("To use the forensic tool, use the following syntax:\nforensic [-r] [-h [md5[,sha1[,sha256]]] [-o <outfile>] [-v] <file|dir>\n");      
+        exit(1);
+    }
     exit(0);
 }
