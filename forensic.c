572c8466bb622e7f3535ecc8fb64f08ec341a858
Leonor Sousa <leonormsousa@sapo.pt>
2019-03-19 15:47:54 +0000

    Added main file

---
+++ forensic.c
@@ -0,0 +1,14 @@
+//Developers: Afonso Mendonça, João Araújo & Leonor Sousa
+//Operative Systems
+
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(int argc, char* argv[])
+{
+    exit(0);
+}
