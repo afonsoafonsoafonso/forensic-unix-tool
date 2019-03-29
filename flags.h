4c8d09d489bf64f23dc329bf7c3fc35b64232a53
Afonso Mendonca <afonsomendonca91@gmail.com>
2019-03-29 19:47:27 +0000

    arg_parser fully working but still needs some tweaking in respect to invalid argumetns handling

--- flags.h
+++ flags.h
@@ -1,14 +1,51 @@
-struct argFlags {
-    int dir_full_search;
-    struct hash_flags hash_flags;
-    int logfile;
-    int outfile;
-    char outfile_name[255]; //255 is, normally, the max file name lenght on UNIX
-    char logfile_name[255];
-};
-
 struct hash_flags {
     int md5;
     int sha1;
     int sha256;
-}
+};
+
+struct argFlags {
+    int dir_full_search;
+    int hash_calc;
+    struct hash_flags hash_flags;
+    int logfile;
+    int outfile;
+    char logfile_name[255];
+    char outfile_name[255]; //255 is, normally, the max file name lenght on UNIX
+};
+/*struct argFlags arg_handler(int argc, char** argv) {
+    struct argFlags arg_flags = {0};
+
+    for(int i=1; i<argc; i++) {
+        if(strcmp(argv[i],"-r")==0) {
+            arg_flags.dir_full_search=1;
+        }
+
+        if(strcmp(argv[i],"-h")==0) {
+            while(strcmp(argv[i],"md5")==0 || strcmp(argv[i],"sha1")==0 || strcmp(argv[i],"sha256")==0) {
+
+                if(strcmp(argv[i],"md5")==0) 
+                    arg_flags.hash_flags.md5==1;
+
+                else if(strcmp(argv[i],"sha1")==0) 
+                    arg_flags.hash_flags.sha1==1;
+                
+                else 
+                    arg_flags.hash_flags.sha256==1;
+
+                i++;
+            }
+
+        }
+        //talvez mudar isto para abrir diretamente o ficheiro e guardar na struct 
+        //num membro do tipo FILE* ???
+        if(strcmp(argv[i],"-o")==0) {
+            strcpy(argv[i],arg_flags.outfile_name);
+        }
+
+        if(strcmp(argv[i],"-v")==0) {
+            strcpy(getenv("LOGFILENAME"),arg_flags.logfile_name);
+        }
+    }
+    return arg_flags;
+}*/
--- forensic.c
+++ forensic.c
@@ -10,7 +10,10 @@
 #include <string.h>
 
 //
-#include <flags.h>
+#include "flags.h"
+
+struct argFlags arg_parser(int argc, char** argv);
+void arg_handler_test(struct argFlags arg_flags);
 
 int main(int argc, char** argv)
 {
@@ -22,43 +25,56 @@ int main(int argc, char** argv)
         exit(1);
     }
 
+    printf("argc: %d\n", argc);
+    arg_flags=arg_parser(argc,argv);
+    arg_handler_test(arg_flags);
+
     exit(0);
 }
 
 // TO-DO: mais tarde mudar esta função (e outras) para outro ficheiro com respetivo header
-int arg_handler(int argc, char** argv, struct argFlags arg_flags) {
-    for(int i=1; i<=argc; i++) {
-
-        if(argv[i]=="-r") {
+//adicionar também verificação de argumentos válidos
+struct argFlags arg_parser(int argc, char** argv) {
+    struct argFlags arg_flags = {0};
+    char *token;
+    
+    for(int i=1; i<argc; i++) {
+        if(strcmp(argv[i],"-r")==0) {
             arg_flags.dir_full_search=1;
         }
 
-        else if(argv[i]=="-h") {
-            while(argv[i]=="md5" || argv[i]=="sha1" || argv[i]=="sha256") {
-
-                if(argv[i]=="md5") 
-                    arg_flags.hash_flags.md5==1;
-
-                else if(argv[i]=="sha1") 
-                    arg_flags.hash_flags.sha1==1;
-                
-                else 
-                    arg_flags.hash_flags.sha256==1;
-
-                i++;
+        if(strcmp(argv[i],"-h")==0) {
+            arg_flags.hash_calc=1;
+            i++;
+            token = strtok(argv[i],",");
+            while(token!=NULL) {
+                if(strcmp(token,"md5")==0)
+                    arg_flags.hash_flags.md5=1;
+                else if(strcmp(token,"sha1")==0)
+                    arg_flags.hash_flags.sha1=1;
+                else if(strcmp(token,"sha256")==0)
+                    arg_flags.hash_flags.sha256=1;
+                token = strtok(NULL,",");
             }
-
-        }
-        //talvez mudar isto para abrir diretamente o ficheiro e guardar na struct 
-        //num membro do tipo FILE* ???
-        else if(argv[i]=="-o") {
-            i++;
-            strcpy(argv[i],arg_flags.outfile_name);
         }
 
-        else if(argv[i]=="-v") {
+        if(strcmp(argv[i],"-o")==0) {
+            arg_flags.outfile=1;
             i++;
-            strcpy(getenv("LOGFILENAME"),arg_flags.logfile_name);
+            strcpy(arg_flags.outfile_name,argv[i]);
+        }
+        if(strcmp(argv[i],"-v")==0) {
+            arg_flags.logfile=1;
+            i++;
+            strcpy(arg_flags.logfile_name,argv[i]);
         }
     }
+
+    return arg_flags;
+}
+
+void arg_handler_test(struct argFlags arg_handler) {
+    printf("-r:%d\n ; -h:%d\n ; -v:%d - %s\n ; -o:%d - %s\n",arg_handler.dir_full_search,arg_handler.hash_calc,arg_handler.logfile,arg_handler.logfile_name,arg_handler.outfile,arg_handler.outfile_name);
+
+    printf("md5:%d ; sha1:%d ; sha256:%d\n",arg_handler.hash_flags.md5,arg_handler.hash_flags.sha1,arg_handler.hash_flags.sha256);
 }
